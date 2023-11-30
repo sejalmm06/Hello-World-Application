f6f11522a0e6c90314b2371c1618e7e218a8e0cb
sejalmm06 <129971092+sejalmm06@users.noreply.github.com>
2023-11-30 16:46:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,27 @@ resource "aws_instance" "k8s_worker" {
     Name = "k8s-worker"
   }
 }
+resource "aws_instance" "prometheus_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with the AMI for your region and OS
+  instance_type = "t2.micro"
+  key_name      = "your-key-pair-name"
+  security_group = "your-security-group-id"
+
+  tags = {
+    Name = "prometheus-server"
+  }
+}
+
+resource "aws_instance" "grafana_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with the AMI for your region and OS
+  instance_type = "t2.micro"
+  key_name      = "your-key-pair-name"
+  security_group = "your-security-group-id"
+
+  tags = {
+    Name = "grafana-server"
+  }
+}
 
 # Output the public IP addresses of the instances
 output "test_server_ip" {
@@ -43,4 +64,11 @@ output "k8s_master_ip" {
 
 output "k8s_worker_ip" {
   value = aws_instance.k8s_worker.public_ip
+}
+output "prometheus_server_ip" {
+  value = aws_instance.prometheus_server.public_ip
+}
+
+output "grafana_server_ip" {
+  value = aws_instance.grafana_server.public_ip
 }
